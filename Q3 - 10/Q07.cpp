150c01efd5ce8f46a5b27d5430dd767fafca5834
Piyush Arora <iit2015071@iiita.ac.in>
2017-04-09 20:10:09 +0530

    Q07 complete

--- Q3 - 10/Q07.cpp
+++ Q3 - 10/Q07.cpp
@@ -1,5 +1,6 @@
 #include "Includes.cpp"
-#include "Q06Utility.cpp"
+#include "Q07Utility.cpp"
+#define MAX 1000
 
 int main()
 {
@@ -9,15 +10,25 @@ int main()
     getBoysList(B);
     getGirlsList(G);
     getCouplesList(C);
-    cout << C.size() << endl << B.size() << endl << G.size() << endl;
+    sortAllByIndex(B, G, C);
+    writeBoysToFile(B);
+    writeGirlsToFile(G);
+    writeCouplesToFile(C);
     int t;
-    cout << "\nEnter t to perform breakups where happiness less than t: ";
+    cout << "\nChoose:\n\t1 for array\n\t2 for binary search\n\t3 for hash table\n\t4 for default\n";
     cin >> t;
-    if(t > 366) {
-        cout << "Enter t less than 366 since an year has maximum 366 days.\nEnter t: ";
+    while(t > 4) {
+        cout << "Invalid value.\nEnter t: ";
         cin >> t;
     }
-    performBreakupLT(B, G, C, t);
+    if(t == 1) {
+        arrayImp(C, B, G);
+    } else if(t == 2) {
+        bs(C, B, G);
+    } else if(t == 3) {
+        hashf(C, B, G);
+    } else if(t == 4) {
+        arrayImp(C, B, G);
+    }
     return 0;
-    cout << "\nLog files updated successfully!\n";
 }
--- Q3 - 10/Q07Utility.cpp
+++ Q3 - 10/Q07Utility.cpp
@@ -1,4 +1,191 @@
 #include <iostream>
 #include <fstream>
+#include <cstring>
 using namespace std;
+#define MAX 1000
 
+void arrayImp(vector<Couple> &C, vector<Boy> &B, vector<Girl> &G)
+{
+    int arr[MAX][2];
+    vector<Couple>::iterator cc;
+    vector<Boy>::iterator b;
+    vector<Girl>::iterator g;
+    int boy;
+    int girl;
+    int i = 0;
+    int c;
+    int choice;
+    int n;
+    int ansF;
+    for(cc = C.begin(); cc != C.end(); cc++) {
+        boy = cc->getBoy();
+        girl = cc->getGirl();
+        arr[i][0] = boy;
+        arr[i++][1] = girl;
+    }
+    n = i;
+    cout << "Choose:\n\t1 for search by index\n\t2 for search by name \n";
+    cin >> choice;
+    if(choice == 1) {
+        cout << "Enter the boy's index to get his girlfriend (Enter 9999 to exit): ";
+        cin >> c;
+        while(c != 9999) {
+            if(c > 1000) {
+                cout << "Invalid value. Exiting.\n";
+                return;
+            } else {
+                ansF = 0;
+                for(i = 0; i < n; i++) {
+                    if(arr[i][0] == c) {
+                        ansF = 1;
+                        b = B.begin() + arr[i][0];
+                        g = G.begin() + arr[i][1];
+                        cout << "\tBoy Name: " << b->getName() << "Girl Name: " << g->getName() << endl;
+                        break;
+                    }
+                }
+                if(ansF == 0)
+                    cout << "\tNo girlfriend!" << endl;
+            }
+            cout << "Enter Next: ";
+            cin >> c;
+        }
+    } else if(choice == 2) {
+        string s;
+        cout << "Enter the boy's name to get his girlfriend (Enter 9999 to exit): ";
+        cin >> s;
+        while(s.compare("9999") != 0) {
+            ansF = 0;
+            for(i = 0; i < n; i++) {
+                b = B.begin() + arr[i][0];
+                if(s.compare(b->getName()) == 0) {
+                    ansF = 1;
+                    g = G.begin() + arr[i][1];
+                    cout << "\tBoy Name: " << b->getName() << " Girl Name: " << g->getName() << endl;
+                    break;
+                }
+            }
+            if(ansF == 0)
+                cout << "\tNo girlfriend or does not exist!" << endl;
+            cout << "Enter Next: ";
+            cin >> s;
+        }
+    } else {
+        cout << "Invalid value. Exiting.\n";
+        return;
+    }
+}
+
+void bs(vector<Couple> &C, vector<Boy> &B, vector<Girl> &G)
+{
+    int arr[MAX][2];
+    vector<Couple>::iterator cc;
+    vector<Boy>::iterator b;
+    vector<Girl>::iterator g;
+    int boy;
+    int girl;
+    int i = 0;
+    int c;
+    int choice;
+    int n;
+    int ansF;
+    int first;
+    int last;
+    int middle;
+    for(cc = C.begin(); cc != C.end(); cc++) {
+        boy = cc->getBoy();
+        girl = cc->getGirl();
+        arr[i][0] = boy;
+        arr[i++][1] = girl;
+    }
+    n = i;
+    qsort(arr, 10, sizeof(*arr),
+        [](const void *arg1, const void *arg2)->int
+        {
+            int const *lhs = static_cast<int const*>(arg1);
+            int const *rhs = static_cast<int const*>(arg2);
+            return (lhs[0] < rhs[0]) ? -1
+                :  ((rhs[0] < lhs[0]) ? 1
+                :  (lhs[1] < rhs[1] ? -1
+                :  ((rhs[1] < lhs[1] ? 1 : 0))));
+        });
+    cout << "Enter the boy's index to get his girlfriend (Enter 9999 to exit): ";
+    cin >> c;
+    while(c != 9999) {
+        if(c > 1000) {
+            cout << "Invalid value. Exiting.\n";
+            return;
+        } else {
+            ansF = 0;
+            first = 0;
+            last = n-1;
+            middle = (first+last)/2;
+            while (first <= last) {
+                if(arr[middle][0] < c) {
+                    first = middle + 1;
+                }
+                else if(arr[middle][0] == c) {
+                    ansF = 1;
+                    b = B.begin() + arr[middle][0];
+                    g = G.begin() + arr[middle][1];
+                    cout << "\tBoy Name: " << b->getName() << " Girl Name: " << g->getName() << endl;
+                    break;
+                }
+                else {
+                    last = middle - 1;
+                }
+                middle = (first + last)/2;
+            }
+            if(first > last) {
+                cout << "\tNo girlfriend!\n";
+            }
+            cout << "Enter Next: ";     
+            cin >> c;
+        }
+    }
+}
+
+void hashf(vector<Couple> &C, vector<Boy> &B, vector<Girl> &G)
+{
+    int arr[MAX];
+    vector<Couple>::iterator cc;
+    vector<Boy>::iterator b;
+    vector<Girl>::iterator g;
+    int boy;
+    int girl;
+    int i = 0;
+    int c;
+    int choice;
+    int n;
+    int ansF;
+    for(i = 0; i < MAX; i++) {
+        arr[i] = 9999;
+    }
+    for(cc = C.begin(); cc != C.end(); cc++) {
+        boy = cc->getBoy();
+        girl = cc->getGirl();
+        arr[boy] = girl;
+        i++;
+    }
+    n = i;
+    cout << "Enter the boy's index to get his girlfriend (Enter 9999 to exit): ";
+    cin >> c;
+    while(c != 9999) {
+        if(c > 1000) {
+            cout << "Invalid value. Exiting.\n";
+            return;
+        } else {
+            if(arr[c] == 9999)
+                cout << "\tNo girlfriend!" << endl;
+            
+            else {
+                    b = B.begin() + c;
+                    g = G.begin() + arr[c];
+                    cout << "\tBoy Name: " << b->getName() << " Girl Name: " << g->getName() << endl;
+            }
+        }
+        cout << "Enter Next: ";
+        cin >> c;   
+    }
+    return;   
+}
